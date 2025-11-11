a611132e0b0dc8ca9291961f90021934dd85c509
YefersonChimayco <chimaycohnj@gmail.com>
2025-11-11 10:00:38 -0500

    ffvvvadvdva

--- src/config/config.php
+++ src/config/config.php
@@ -3,14 +3,13 @@
 const BD_HOST = 'localhost';
 const BD_NAME = 'cliente_api';
 const BD_USER = 'root';
-const BD_PASSWORD = '';
+const BD_PASSWORD = 'root';
 
 const BD_CHARSET = 'utf8';
 
 //ingresar link de proyecto
 const BASE_URL = 'http://localhost/token/';
 const BASE_URL_SERVER = 'http://localhost/token/';
-
 const RUTA_API = 'http://localhost/sire2/';
 
 ?>
